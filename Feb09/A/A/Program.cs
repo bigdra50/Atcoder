787a8291fe62d28811cf134d7a748d9ab7b869bf
bigdra50 <ryu1998tea@gmail.com>
2020-02-10 01:12:13 +0900

    add Feb09 Contest

---
+++ Feb09/A/A/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://atcoder.jp/contests/abc154/tasks/abc154_a
+using System;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new StreamWriter(OpenStandardOutput()){AutoFlush = false};
+            SetOut(sw);
+            var st = ReadLine().Split(' ');
+            var ab = ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var u = ReadLine();
+            if (u == st.First())
+            {
+                //ab.First()--;
+                ab[0]--;
+            }
+            else
+            {
+                ab[1]--;
+            }
+            WriteLine($"{ab[0]} {ab[1]}");
+            
+            Out.Flush();
+        }
+    }
+}
---
+++ Feb09/B/B/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://atcoder.jp/contests/abc154/tasks/abc154_b
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Regex.Replace(ReadLine(), ".", "x"));
+        }
+    }
+}
